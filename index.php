8645a881441f61589ebdd29eaf8b4eb10cedf2a8
JPedraza <printepolis@gmail.com>
2018-02-14 22:34:57 -0600

    probando cambios

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <frameset rows="100%">
-	<frame src="index2.php">  
+	<frame src="index2.php">
 </frameset>
 
 </html>
